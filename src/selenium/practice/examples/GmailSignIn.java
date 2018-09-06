b4311f4b1d58dec5426e2a54e0efc5fb1404f270
Pooja Dwarkunde <poojadwarkunde34@gmail.com>
2018-09-07 00:45:24 +0530

    Minor Changes

--- src/selenium/practice/examples/GmailSignIn.java
+++ src/selenium/practice/examples/GmailSignIn.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+
 public class GmailSignIn {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","E:\\owno\\for backup\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
