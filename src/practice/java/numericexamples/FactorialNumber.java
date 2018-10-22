0ae97e5c717b4ae30ac24b4c78aa87a2fe46d804
Pooja <pooja@sagarborse.local>
2018-10-22 22:42:38 +0530

    Refactoring

--- src/practice/java/numericexamples/FactorialNumber.java
+++ src/practice/java/numericexamples/FactorialNumber.java
@@ -3,21 +3,34 @@ package practice.java.numericexamples;
 public class FactorialNumber {
 
 	public static void main(String[] args) {
-		factorial(5);
+		System.out.println(factorial(5));
 		System.out.println(rec_fact(5));
+		System.out.println(factWhile(5));
 	}
 
-	private static int rec_fact(int i) {
-		
-		return i;
-	}
-
-	private static void factorial(int num) {
+	private static int factWhile(int num) {
 		int fact = 1;
-		for(int i=2;i<=num;i++) {
-			fact= fact*i;
+		int i = 2;
+		while (i <= num) {
+			fact = fact * i;
+			i++;
 		}
-		System.out.println(fact);
+		return fact;
+	}
+
+	private static int rec_fact(int num) {
+		if (num == 1) {
+			return 1;
+		}
+		return num * rec_fact(num - 1);
+	}
+
+	private static int factorial(int num) {
+		int fact = 1;
+		for (int i = 2; i <= num; i++) {
+			fact = fact * i;
+		}
+		return fact;
 	}
 
 }
